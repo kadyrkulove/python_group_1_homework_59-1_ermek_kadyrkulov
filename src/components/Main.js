7c12f121364d39ea5a3be7382f01ad0546c1c468
Ermek K <kadyrkulove@gmail.com>
2019-02-21 15:59:53 +0600

    add components/Main.js(Main Form),

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react';
+
+class Main extends Component {
+    shouldComponentUpdate(nextProps){
+    return  nextProps.film !== this.props.film
+  }
+  render() {
+        console.log(this.props.film)
+      return (
+          <Fragment>
+              <input type="text" onChange={(event)=> this.props.change(event)} value={this.props.film}/>
+              <button onClick={(index)=> this.props.remove(index)}>X</button>
+          </Fragment>
+      );
+  }
+}
+
+export default Main;
